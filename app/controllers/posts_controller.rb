13ecc93997898be098b6c91cae1ebfe957895cdc
hamada456 <n87.takaya.hamada@gmail.com>
2020-12-15 19:28:36 +0900

    AjaxApp準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,12 @@
 class PostsController < ApplicationController
 
-  def index  # indexアクションを定義した
-    @posts = Post.all
+  def index
+    @posts = Post.all.order(id: "DESC") #新しいメモが一番上に表示、DESC（降順）
   end
-  
-def new
-end
 
-def create
-  Post.create(content: params[:content])
-end
+  def create
+    Post.create(content: params[:content])
+    redirect_to action: :index  #メモを保存した後にトップページへリダイレクト
+  end
+
 end
