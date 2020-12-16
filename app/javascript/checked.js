b7f040774451571669e6737beeadfe952d1510ed
hamada456 <n87.takaya.hamada@gmail.com>
2020-12-17 05:13:37 +0900

    「メモクリックのイベント時」に動作

--- app/javascript/checked.js
+++ app/javascript/checked.js
@@ -1,4 +1,7 @@
 function check() {
   const posts = document.querySelectorAll(".post");
+  posts.forEach(function (post) {
+    post.addEventListener("click", () => { });
+   });
 }
 window.addEventListener("load", check);
