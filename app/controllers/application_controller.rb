cc068187434dd203828b2d00aa0b855bbfd5c344
hamada456 <n87.takaya.hamada@gmail.com>
2021-01-11 11:42:20 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth
+#   before_action :basic_auth
 
 
-  private
-#Basic認証
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
-end
-# username == '????' && password == '????' #↑環境変数を読み込む記述に変更
+#   private
+# #Basic認証
+#   def basic_auth
+#     authenticate_or_request_with_http_basic do |username, password|
+#       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+#     end
+#   end
+# end
+# # username == '????' && password == '????' #↑環境変数を読み込む記述に変更
